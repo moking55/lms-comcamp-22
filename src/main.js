303076f66e4ffc0aefab9208a89be738f35d100c
moking55 <moking55@hotmail.com>
2023-10-09 16:46:44 +0700

    lib: initial firebase

--- src/main.js
+++ src/main.js
@@ -10,6 +10,8 @@ import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
 import "@mdi/font/css/materialdesignicons.css";
 import VueConfetti from "vue-confetti";
+// import firebase.js in ./src/firebase.js
+import "./firebase";
 
 
 const vuetify = createVuetify({
